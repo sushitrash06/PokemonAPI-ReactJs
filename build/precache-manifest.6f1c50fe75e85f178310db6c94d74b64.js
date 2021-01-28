1b6b900fffc24b9fae0d8a5f85dd46eba3d991af
Azka Andya <azkaandyaaa@gmail.com>
2021-01-28 13:46:23 +0700

    re-build

---
+++ build/precache-manifest.6f1c50fe75e85f178310db6c94d74b64.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "05acb461cb01fab465c3",
+    "url": "/static/css/main.d70d2224.chunk.css"
+  },
+  {
+    "revision": "05acb461cb01fab465c3",
+    "url": "/static/js/main.05acb461.chunk.js"
+  },
+  {
+    "revision": "5ffd5276e5dbb2dc5510",
+    "url": "/static/css/1.bff83fe7.chunk.css"
+  },
+  {
+    "revision": "5ffd5276e5dbb2dc5510",
+    "url": "/static/js/1.5ffd5276.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f9115592630cc9edfaf0d7c1ee44486d",
+    "url": "/static/media/background.f9115592.jpg"
+  },
+  {
+    "revision": "6e190eb7b580983ce09c7ccf0c91519d",
+    "url": "/static/media/pika_run.6e190eb7.gif"
+  },
+  {
+    "revision": "2d667e9ff12c17b9b123220c788789d2",
+    "url": "/index.html"
+  }
+];
